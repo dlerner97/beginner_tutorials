006b3e0ab9f4ddc31029b4d55c07639befaddeb1
Dani Lerner <dlerner97@gmail.com>
2021-10-30 20:16:40 -0400

    Fixed static code check tests

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,6 +1,16 @@
+/**
+ * @file listener.cpp
+ * @author Dani Lerner (dalerner@umd.edu)
+ * @brief Listener/subscriber node 
+ * @version 0.1
+ * @date 2021-10-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-
 
 void chatterCallback(const std_msgs::String::ConstPtr& msg) {
   ROS_INFO_STREAM("I heard: [" << msg->data << "]");
--- src/talker.cpp
+++ src/talker.cpp
@@ -1,7 +1,18 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
+/**
+ * @file talker.cpp
+ * @author Dani Lerner (dalerner@umd.edu)
+ * @brief Talker/publisher node
+ * @version 0.1
+ * @date 2021-10-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
 #include <string>
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
@@ -16,7 +27,9 @@ int main(int argc, char **argv) {
   int count = 0;
   while (ros::ok()) {
     std_msgs::String msg;
-    msg.data = std::string("This is Dani's tutorial. TBH, I've done it wayyy too many times. " + std::to_string(count));
+    msg.data = std::string(
+        "This is Dani's tutorial. TBH, I've done it wayyy too many times. " +
+        std::to_string(count));
 
     ROS_INFO_STREAM(msg.data);
 
