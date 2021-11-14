d38e65b700dfe27a9ba3165fa0263265259ded22
Dani Lerner <dlerner97@gmail.com>
2021-11-14 12:55:29 -0500

    Created testing class for the talker node. Acts as a listener and gets some metrics from subscription

---
+++ include/talker_test_class.hpp
@@ -0,0 +1,39 @@
+ /**
+ * @file talker_test_class.hpp
+ * @author Dani Lerner (dalerner@umd.edu)
+ * @brief Test class for talker class declaration
+ * @version 0.1
+ * @date 2021-10-30
+ * 
+ * @copyright Copyright (c) 2021. All rights reserved.
+ * This project is released under the MIT Public License.
+ */
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+class TalkerTest { 
+
+    int _num_msgs{0};
+    bool _first_msg{true};
+    ros::Duration _sum;
+    ros::Time _prev_msg_time;
+    ros::Subscriber _chatter_sub;
+
+    /**
+     * @brief Callback method for the chatter subscriber
+     * 
+     * @param msg 
+     */
+    void chatterCallback(const std_msgs::String::ConstPtr& msg);
+ 
+ public:
+    TalkerTest(ros::NodeHandle* n) : _sum(0.0) {
+        _chatter_sub = n->subscribe("chatter", 1000, &TalkerTest::chatterCallback, this);
+
+        ROS_DEBUG_STREAM("Listener node initialized.");
+        _prev_msg_time = ros::Time::now();
+    }
+
+    double get_avg_rate(void);
+};
